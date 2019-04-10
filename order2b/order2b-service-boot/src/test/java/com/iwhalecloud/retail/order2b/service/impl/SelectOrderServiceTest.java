e41d780b6a2b79cbf0d83ca2fe25e90fefadf17c
gong.sheng <gong.sheng@iwhalecloud.com>
2019-04-10 16:14:03 +0800

    优化查询

--- order2b/order2b-service-boot/src/test/java/com/iwhalecloud/retail/order2b/service/impl/SelectOrderServiceTest.java
+++ order2b/order2b-service-boot/src/test/java/com/iwhalecloud/retail/order2b/service/impl/SelectOrderServiceTest.java
@@ -69,9 +69,10 @@ public class SelectOrderServiceTest extends TestBase {
         AdvanceOrderReq req=new AdvanceOrderReq();
         req.setUserExportType("3");
         req.setOrderCat("0");
+        req.setUserCode("4301811022885");
+        req.setUserId("1077839559879852033");
         req.setSourceFrom("YHJ");
         SourceFromContext.setSourceFrom("YHJ");
-        req.setOrderId("201903129710000440");
         orderSelectService.orderExport(req);
     }
 

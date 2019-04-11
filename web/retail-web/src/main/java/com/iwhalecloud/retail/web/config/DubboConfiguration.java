c03c6139c924de754a68d3d16e057ba1709e5ada
shi.zhuxiong <0027007822@iwhalecloud.com>
2019-04-11 10:22:49 +0800

    Revert "fix:将web超时时间设置为15秒"
    This reverts commit f047a4f82704c5d25efe565d0469aa02a890ebf5.

--- web/retail-web/src/main/java/com/iwhalecloud/retail/web/config/DubboConfiguration.java
+++ web/retail-web/src/main/java/com/iwhalecloud/retail/web/config/DubboConfiguration.java
@@ -17,8 +17,7 @@ public class DubboConfiguration {
 	public ConsumerConfig consumerConfig() {
 	   ConsumerConfig consumerConfig = new ConsumerConfig();
 	   consumerConfig.setCheck(false);
-	   //web的超时时长设置为15秒
-	   consumerConfig.setTimeout(15000);
+	   consumerConfig.setTimeout(5000);
 	   consumerConfig.setRetries(0); 	//不重发请求
 	   consumerConfig.setFilter("sourceFromfilter");
 	   return consumerConfig;

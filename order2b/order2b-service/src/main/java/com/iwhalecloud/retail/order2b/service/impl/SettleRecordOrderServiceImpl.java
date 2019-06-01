c71c5231e38683df1db044a95dd94065861de5a7
peng.xiang <peng.xiang@iwhalecloud.com>
2019-06-01 17:08:12 +0800

    同步修改

--- order2b/order2b-service/src/main/java/com/iwhalecloud/retail/order2b/service/impl/SettleRecordOrderServiceImpl.java
+++ order2b/order2b-service/src/main/java/com/iwhalecloud/retail/order2b/service/impl/SettleRecordOrderServiceImpl.java
@@ -5,11 +5,16 @@ import com.iwhalecloud.retail.order2b.dto.model.order.SettleRecordOrderDTO;
 import com.iwhalecloud.retail.order2b.manager.DeliveryManager;
 import com.iwhalecloud.retail.order2b.service.SettleRecordOrderService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.MapUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2019/3/29.
@@ -23,6 +28,29 @@ public class SettleRecordOrderServiceImpl implements SettleRecordOrderService {
 
     @Override
     public List<SettleRecordOrderDTO> getSettleRecordOrder(List<String> orderIds,String lanId) {
-        return deliveryManager.getSettleRecordOrder(orderIds,lanId);
+        List<SettleRecordOrderDTO> settleRecordOrderDTOs = deliveryManager.getSettleRecordOrder(orderIds,lanId);
+        if(!CollectionUtils.isEmpty(settleRecordOrderDTOs)){
+            Map<String,Integer> orderIdNumMap = this.frequencyOfListElements(settleRecordOrderDTOs);
+            if(MapUtils.isNotEmpty(orderIdNumMap)){
+                for(SettleRecordOrderDTO settleRecordOrderDTO : settleRecordOrderDTOs){
+                    String orderId = settleRecordOrderDTO.getOrderId();
+                    if(orderIdNumMap.containsKey(orderId)){
+                        settleRecordOrderDTO.setCouponPrice(settleRecordOrderDTO.getCouponPrice()/orderIdNumMap.get(orderId));
+                    }
+                }
+            }
+        }
+
+        return settleRecordOrderDTOs;
+    }
+
+    private static  Map<String,Integer> frequencyOfListElements(List<SettleRecordOrderDTO> settleRecordOrderDTOs){
+        if (settleRecordOrderDTOs == null || settleRecordOrderDTOs.size() == 0) return null;
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (SettleRecordOrderDTO settleRecordOrderDTO : settleRecordOrderDTOs) {
+            Integer count = map.get(settleRecordOrderDTO.getOrderId());
+            map.put(settleRecordOrderDTO.getOrderId(), (count == null) ? 1 : count + 1);
+        }
+        return map;
     }
 }

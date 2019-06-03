ae4898f2c525028dc446faef6a29fd10b8351774
zhong.wenlong <zhong.wenlong30@iwhalecloud.com>
2019-06-03 16:21:03 +0800

    商品类型条件 相关功能

--- goods2b/goods2b-service/src/main/java/com/iwhalecloud/retail/goods2b/service/impl/dubbo/CatConditionServiceImpl.java
+++ goods2b/goods2b-service/src/main/java/com/iwhalecloud/retail/goods2b/service/impl/dubbo/CatConditionServiceImpl.java
@@ -1,19 +1,67 @@
 package com.iwhalecloud.retail.goods2b.service.impl.dubbo;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.iwhalecloud.retail.dto.ResultVO;
+import com.iwhalecloud.retail.goods2b.dto.CatConditionDTO;
+import com.iwhalecloud.retail.goods2b.dto.req.CatConditionListReq;
+import com.iwhalecloud.retail.goods2b.dto.req.CatConditionSaveReq;
+import com.iwhalecloud.retail.goods2b.entity.CatCondition;
 import com.iwhalecloud.retail.goods2b.manager.CatConditionManager;
 import com.iwhalecloud.retail.goods2b.service.dubbo.CatConditionService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Date;
+import java.util.List;
 
+@Slf4j
 @Service
 public class CatConditionServiceImpl implements CatConditionService {
 
     @Autowired
     private CatConditionManager catConditionManager;
 
-    
-    
-    
-    
+    /**
+     * 添加一个 商品类型条件
+     * @param req
+     * @return
+     */
+    @Override
+    public ResultVO<Integer> saveCatCondition(CatConditionSaveReq req) {
+        log.info("CatConditionServiceImpl.saveCatCondition() input: {}", JSON.toJSONString(req));
+        ResultVO<Integer> resultVO = new ResultVO<>();
+        CatCondition entity = new CatCondition();
+        BeanUtils.copyProperties(req, entity);
+        // 其他信息设置
+        entity.setCreateDate(new Date());
+        entity.setUpdateDate(new Date());
+        if (!StringUtils.isEmpty(req.getCreateStaff())) {
+            entity.setUpdateStaff(req.getCreateStaff());
+        }
+        Integer resultInt = catConditionManager.saveCatCondition(entity);
+        if (resultInt > 0) {
+            resultVO = ResultVO.success(resultInt);
+        } else {
+            resultVO = ResultVO.error();
+        }
+        log.info("CatConditionServiceImpl.saveCatCondition() output: {}", JSON.toJSONString(resultVO));
+        return resultVO;
+    }
+
+    /**
+     * 商品类型条件 列表查询
+     * @param req
+     * @return
+     */
+    @Override
+    public ResultVO<List<CatConditionDTO>> listCatCondition(CatConditionListReq req) {
+        log.info("CatConditionServiceImpl.listCatCondition() input: {}", JSON.toJSONString(req));
+        ResultVO<List<CatConditionDTO>> resultVO = ResultVO.success(catConditionManager.listCatCondition(req));
+        log.info("CatConditionServiceImpl.listCatCondition() output: {}", JSON.toJSONString(resultVO));
+        return resultVO;
+    }
+
 }
---
+++ web/retail-web/src/main/java/com/iwhalecloud/retail/web/controller/b2b/goods/CatConditionB2BController.java
@@ -0,0 +1,68 @@
+package com.iwhalecloud.retail.web.controller.b2b.goods;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.fastjson.JSON;
+import com.iwhalecloud.retail.dto.ResultVO;
+import com.iwhalecloud.retail.goods2b.dto.CatConditionDTO;
+import com.iwhalecloud.retail.goods2b.dto.req.CatConditionListReq;
+import com.iwhalecloud.retail.goods2b.dto.req.CatConditionSaveReq;
+import com.iwhalecloud.retail.goods2b.service.dubbo.CatConditionService;
+import io.swagger.annotations.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author wenlong.zhong
+ * @date 2019/6/3
+ */
+@RestController
+@Slf4j
+@RequestMapping("/api/b2b/catCondition")
+@Api(value = "商品类型条件控制器", tags = {"商品类型条件控制器"})
+public class CatConditionB2BController {
+
+    @Reference
+    private CatConditionService catConditionService;
+
+    /**
+     * 添加一个 商品类型条件
+     * @param req
+     * @return
+     */
+    @ApiOperation(value = "添加一个 商品类型条件 记录", notes = "添加一个 商品类型条件 记录")
+    @ApiResponses({
+            @ApiResponse(code = 400, message = "请求参数没填好"),
+            @ApiResponse(code = 404, message = "请求路径没有或页面跳转路径不对")
+    })
+    @PostMapping(value = "/save")
+    public ResultVO<Integer> saveCatCondition(@RequestBody @ApiParam(value = "添加一个 商品类型条件 记录的入参数", required = true) CatConditionSaveReq req) {
+        log.info("CatConditionB2BController.saveCatCondition() input:req={} ", JSON.toJSONString(req));
+        ResultVO resultVO = catConditionService.saveCatCondition(req);
+        log.info("CatConditionB2BController.saveCatCondition() out:resultVO={} ", JSON.toJSONString(resultVO));
+        return resultVO;
+    }
+
+    /**
+     * 商品类型条件 列表查询
+     * @param req
+     * @return
+     */
+    @ApiOperation(value = "商品类型条件 列表查询", notes = "商品类型条件 列表查询")
+    @ApiResponses({
+            @ApiResponse(code = 400, message = "请求参数没填好"),
+            @ApiResponse(code = 404, message = "请求路径没有或页面跳转路径不对")
+    })
+    @PostMapping(value = "/list")
+    public ResultVO<List<CatConditionDTO>> listCatCondition(@RequestBody @ApiParam(value = "商品类型条件 列表查询的入参数", required = true) CatConditionListReq req) {
+        log.info("CatConditionB2BController.listCatCondition() input:req={} ", JSON.toJSONString(req));
+        ResultVO resultVO = catConditionService.listCatCondition(req);
+        log.info("CatConditionB2BController.listCatCondition() out:resultVO={} ", JSON.toJSONString(resultVO));
+        return resultVO;
+    }
+
+}

903a343080e3e48ad6866846ba3a29ef7acd053d
liweisong <6027023377@iwhalecloud.com>
2019-06-18 20:35:29 +0800

    报表

--- report/report-client/src/main/java/com/iwhalecloud/retail/report/dto/request/ReportStSaleDaoReq.java
+++ report/report-client/src/main/java/com/iwhalecloud/retail/report/dto/request/ReportStSaleDaoReq.java
@@ -9,22 +9,17 @@ public class ReportStSaleDaoReq extends PageVO {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	  private String brand;//品牌
-	  private String city;//区县
-	  private String createTimeEnd;//入库起止时间
-	  private String createTimeStart;
-	  private String businessEntityName;//经营主体名称
-	  private String lanId;//地市
-	  private String retailerCode;//零售商编号
-	  private String retailerName;//零售商名称
-	  private String outTimeEnd;//出库起止时间
-	  private String outTimeStart;
-	  private String productType;//机型
-	  private String warningStatus;//库存预警状态
-	  private String userType;
-	  private String userId;
-	  private String typeId;//产品类型
-	  private String legacyAccount;//零售商归属原系统还是云货架
-
-
+	private String createTimeStart;//入库起止时间
+	private String createTimeEnd;//入库起止时间
+	private String outTimeEnd;//出库起止时间
+	private String outTimeStart;//出库起止时间
+	private String brandName; 		//品牌
+	private String productBaseName; 	//机型
+	private String partnerName; 	//零售商名称
+	private String partnerCode; 	//零售商编码
+	private String businessEntityName; //经营主体名称
+	private String typeName; 		//产品类型
+	private String cityId; 		//地市
+	private String countyId; 		//区县
+	private String stockWarning; 	//库存预警
 }
--- web/retail-web/src/main/java/com/iwhalecloud/retail/web/controller/report/ReportStoreController.java
+++ web/retail-web/src/main/java/com/iwhalecloud/retail/web/controller/report/ReportStoreController.java
@@ -16,9 +16,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.iwhalecloud.retail.dto.ResultVO;
 import com.iwhalecloud.retail.oms.OmsCommonConsts;
+import com.iwhalecloud.retail.partner.dto.MerchantDTO;
+import com.iwhalecloud.retail.partner.service.MerchantService;
 import com.iwhalecloud.retail.report.dto.request.ReportDeSaleDaoReq;
 import com.iwhalecloud.retail.report.service.IReportDataInfoService;
 import com.iwhalecloud.retail.report.service.ReportService;
@@ -38,11 +41,14 @@ import com.iwhalecloud.retail.report.dto.response.ReportStorePurchaserResq;
 import com.iwhalecloud.retail.report.dto.request.ReportStSaleDaoReq;
 import com.iwhalecloud.retail.report.dto.request.ReportStorePurchaserReq;
 import com.iwhalecloud.retail.report.service.ReportStoreService;
+import com.iwhalecloud.retail.system.common.SystemConst;
+import com.iwhalecloud.retail.warehouse.dto.ResouceStoreDTO;
+import com.iwhalecloud.retail.warehouse.dto.request.StorePageReq;
 
 /**
  * @author zwl
  * @date 2018-11-09
- * （厂家）门店报表
+ * 门店报表
  */
 @Slf4j
 @RestController
@@ -54,7 +60,7 @@ public class ReportStoreController extends BaseController {
     private ReportStoreService reportStoreService;
     
     @Reference
-    private IReportDataInfoService iReportDataInfoService;
+	private MerchantService merchantService;
     
     @Autowired
     private DeliveryGoodsResNberExcel deliveryGoodsResNberExcel;
@@ -67,21 +73,21 @@ public class ReportStoreController extends BaseController {
     @PostMapping("/getReportStSaleList")
 	@UserLoginToken
     public ResultVO<Page<ReportStSaleDaoResp>> getReportStSaleList(@RequestBody ReportStSaleDaoReq req) {
-//		String legacyAccount = req.getLegacyAccount();//判断是云货架还是原系统的零售商，默认云货架
-//		String retailerCodes = req.getRetailerCode();//是否输入了零售商账号
-		String userType=req.getUserType();
-		//String userType = UserContext.getUser().getUserFounder()+"";
-//		if("2".equals(legacyAccount) && !"3".equals(userType) && retailerCodes != null){
-//			retailerCodes = iReportDataInfoService.retailerCodeBylegacy(retailerCodes);
-//			req.setRetailerCode(retailerCodes);
-//		}
-		
-//		String userId = UserContext.getUserId();
-//		ReportStorePurchaserReq req2 = new ReportStorePurchaserReq();
-//		req2.setUserId(userId);
-//		ResultVO<List<ReportStorePurchaserResq>> brandview = iReportDataInfoService.getUerRoleForView(req2);
-//		String brandId = brandview.getResultData().get(0).getBrandId();
-		//req.setBrand(brandId);
+		log.info("****************************ReportStoreController getReportStSaleList    req={}",JSON.toJSONString(req));
+		int userType=UserContext.getUser().getUserFounder();
+		if(userType == SystemConst.USER_FOUNDER_1  || userType == SystemConst.USER_FOUNDER_2) {//超级管理员  省管理员
+		} else if (userType == SystemConst.USER_FOUNDER_9) {//地市管理员
+			String lanId=UserContext.getUser().getLanId();
+			req.setCityId(lanId);
+		} else if (userType == SystemConst.USER_FOUNDER_3 ) {//零售商 （只能查看自己的仓库）
+			MerchantDTO merchantDTO = merchantService.getMerchantInfoById(UserContext.getUser().getRelCode());
+			if ( merchantDTO == null) {
+				return ResultVO.error("当前用户 没有商家编码");
+			}
+			req.setPartnerCode(merchantDTO.getMerchantCode());
+		} else {
+			return ResultVO.error("当前用户 没有权限");
+		}
         return reportStoreService.getReportStSaleList(req);
     }
 	
@@ -96,14 +102,21 @@ public class ReportStoreController extends BaseController {
     @PostMapping(value="/cjStorePurchaserReportExport")
     @UserLoginToken
     public void cjStorePurchaserReportExport(@RequestBody ReportStSaleDaoReq req, HttpServletResponse response) {
-//    	String legacyAccount = req.getLegacyAccount();//判断是云货架还是原系统的零售商，默认云货架
-//		String retailerCodes = req.getRetailerCode();//是否输入了零售商账号
-		String userType=req.getUserType();
-		//String userType = UserContext.getUser().getUserFounder()+"";
-//		if("2".equals(legacyAccount) && !"3".equals(userType) && retailerCodes != null){
-//			retailerCodes = iReportDataInfoService.retailerCodeBylegacy(retailerCodes);
-//			req.setRetailerCode(retailerCodes);
-//		}
+    	int userType=UserContext.getUser().getUserFounder();
+		if(userType == SystemConst.USER_FOUNDER_1  || userType == SystemConst.USER_FOUNDER_2) {//超级管理员  省管理员
+		} else if (userType == SystemConst.USER_FOUNDER_9) {//地市管理员
+			String lanId=UserContext.getUser().getLanId();
+			req.setCityId(lanId);
+		} else if (userType == SystemConst.USER_FOUNDER_3 ) {//零售商 （只能查看自己的仓库）
+			MerchantDTO merchantDTO = merchantService.getMerchantInfoById(UserContext.getUser().getRelCode());
+			if ( merchantDTO == null) {
+				return ;
+			}
+			req.setPartnerCode(merchantDTO.getMerchantCode());
+		} else {
+			return ;
+		}
+		
         ResultVO<List<ReportStSaleDaoResp>> resultVO = reportStoreService.getReportStSaleListdc(req);
         ResultVO result = new ResultVO();
         if (!resultVO.isSuccess()) {
@@ -117,64 +130,39 @@ public class ReportStoreController extends BaseController {
         Workbook workbook = new HSSFWorkbook();
         
         List<ExcelTitleName> orderMap = new ArrayList<>();
-	        orderMap.add(new ExcelTitleName("partnerCode", "零售商编码"));
-	        orderMap.add(new ExcelTitleName("partnerName", "零售商名称"));
-	        orderMap.add(new ExcelTitleName("businessEntityName", "所属经营主体"));
-	        orderMap.add(new ExcelTitleName("date", "统计日期"));
-	        orderMap.add(new ExcelTitleName("cityId", "所属城市"));
-	        orderMap.add(new ExcelTitleName("countryId", "所属区县"));
+        orderMap.add(new ExcelTitleName("partnerName", "零售商名称"));
+        orderMap.add(new ExcelTitleName("partnerCode", "零售商编码"));
+        orderMap.add(new ExcelTitleName("businessEntityName", "所属经营主体"));
+        orderMap.add(new ExcelTitleName("typeName", "产品类型"));
+        orderMap.add(new ExcelTitleName("stockWarning", "库存预警"));
+        orderMap.add(new ExcelTitleName("date", "统计日期"));
+        orderMap.add(new ExcelTitleName("cityId", "所属城市"));
+        orderMap.add(new ExcelTitleName("countryId", "所属区县"));
         orderMap.add(new ExcelTitleName("productBaseName", "机型"));
         orderMap.add(new ExcelTitleName("brandName", "品牌"));
-        orderMap.add(new ExcelTitleName("typeId", "产品类型"));
-        orderMap.add(new ExcelTitleName("theTotalInventory", "入库总量"));
-        orderMap.add(new ExcelTitleName("theTotalOutbound", "出库总量"));
-        orderMap.add(new ExcelTitleName("stockTotalNum", "库存总量"));
+        orderMap.add(new ExcelTitleName("priceLevel", "机型档位"));
+        orderMap.add(new ExcelTitleName("totalInNum", "入库总量"));
+        orderMap.add(new ExcelTitleName("totalOutNum", "出库总量"));
         orderMap.add(new ExcelTitleName("purchaseNum", "交易入库量"));
         orderMap.add(new ExcelTitleName("manualNum", "手工入库量"));
-        	orderMap.add(new ExcelTitleName("transInNum", "调拨入库量"));
-        	orderMap.add(new ExcelTitleName("purchaseAmount", "进货金额"));
-        orderMap.add(new ExcelTitleName("totalSalesNum", "总销售量"));
-            orderMap.add(new ExcelTitleName("sellAmount", "总销售额"));
-        orderMap.add(new ExcelTitleName("manualSalesNum", "手工销售量"));
-        orderMap.add(new ExcelTitleName("crmcontractNum", "CRM合约销量"));
+        orderMap.add(new ExcelTitleName("transInNum", "调拨入库量"));
+    	orderMap.add(new ExcelTitleName("purchaseAmount", "进货金额"));
+    	orderMap.add(new ExcelTitleName("sellNum", "总销售量"));
+        orderMap.add(new ExcelTitleName("sellAmount", "总销售额"));
+        orderMap.add(new ExcelTitleName("uncontractNum", "手工销售量"));
+        orderMap.add(new ExcelTitleName("contractNum", "CRM合约销量"));
         orderMap.add(new ExcelTitleName("registerNum", "自注册销量"));
+        orderMap.add(new ExcelTitleName("transOutNum", "调拨出库量"));
         orderMap.add(new ExcelTitleName("returnNum", "退库量"));
-        orderMap.add(new ExcelTitleName("averageDailySales", "近7天日均销量"));
-        	orderMap.add(new ExcelTitleName("stockAmount", "库存金额"));
+        orderMap.add(new ExcelTitleName("weekAvgSellNum", "近7天日均销量"));
+        orderMap.add(new ExcelTitleName("stockAmount", "库存金额"));
         orderMap.add(new ExcelTitleName("stockNum", "库存量"));
-        orderMap.add(new ExcelTitleName("stockTurnover", "库存周转率"));
-        orderMap.add(new ExcelTitleName("inventoryWarning", "库存预警"));
+        orderMap.add(new ExcelTitleName("turnoverRate", "库存周转率"));
         
       //创建orderItemDetail
         deliveryGoodsResNberExcel.builderOrderExcel(workbook, data,
         		orderMap, "门店进销存明细报表");
         deliveryGoodsResNberExcel.exportExcel("门店进销存明细报表",workbook,response);
-        
-//        return deliveryGoodsResNberExcel.uploadExcel(workbook);
-//        OutputStream output = null ;
-//        try{
-//            //创建Excel
-//            String fileName = "门店进销存明细报表";
-////            ExcelToNbrUtils.builderOrderExcel(workbook, data, orderMap, false);
-//            ExcelToMerchantListUtils.builderOrderExcel(workbook, data, orderMap);
-//            output = response.getOutputStream();
-//            response.reset();
-//            response.setHeader("Content-disposition", "attachment; filename=" + fileName + ".xls");
-//            response.setContentType("application/msexcel;charset=UTF-8");
-//            response.setCharacterEncoding("UTF-8");
-//            workbook.write(output);
-////            output.close();
-//        }catch (Exception e){
-//            log.error("门店进销存明细报表导出失败",e);
-//        } finally {
-//            if (null != output){
-//                try {
-//                    output.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
         
     }
 

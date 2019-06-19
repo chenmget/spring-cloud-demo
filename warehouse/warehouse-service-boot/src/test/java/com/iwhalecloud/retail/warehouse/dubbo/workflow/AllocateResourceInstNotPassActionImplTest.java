3490746470b0251bf26c548b0869c21c6499e408
he.shaowu <he.shaowu01@iwhalecloud.com>
2019-06-19 16:39:57 +0800

    fix 调拨流程服务

--- warehouse/warehouse-service-boot/src/test/java/com/iwhalecloud/retail/warehouse/dubbo/workflow/AllocateResourceInstNotPassActionImplTest.java
+++ warehouse/warehouse-service-boot/src/test/java/com/iwhalecloud/retail/warehouse/dubbo/workflow/AllocateResourceInstNotPassActionImplTest.java
@@ -1,13 +1,14 @@
 package com.iwhalecloud.retail.warehouse.dubbo.workflow;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.iwhalecloud.retail.warehouse.service.AuditNotPassActionService;
 import com.iwhalecloud.retail.workflow.config.InvokeRouteServiceRequest;
 import org.junit.Test;
 
 public class AllocateResourceInstNotPassActionImplTest {
 
     @Reference
-    private AllocateResourceInstNotPassActionService service;
+    private AuditNotPassActionService service;
 
     @Test
     public void run() {

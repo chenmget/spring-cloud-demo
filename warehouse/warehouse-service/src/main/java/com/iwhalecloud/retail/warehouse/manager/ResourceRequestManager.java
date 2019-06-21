f3623c4870725285453ca2985ca91f24ef4b683f
chenmin <chen.min55@iwhalecloud.com>
2019-06-21 14:28:26 +0800

    修改批量审核串码bug

--- warehouse/warehouse-service/src/main/java/com/iwhalecloud/retail/warehouse/manager/ResourceRequestManager.java
+++ warehouse/warehouse-service/src/main/java/com/iwhalecloud/retail/warehouse/manager/ResourceRequestManager.java
@@ -55,7 +55,6 @@ public class ResourceRequestManager{
     public int updateResourceRequestState(ResourceRequestUpdateReq req){
         QueryWrapper queryWrapper = new QueryWrapper();
         queryWrapper.eq(ResourceRequest.FieldNames.mktResReqId.getTableFieldName(),req.getMktResReqId());
-        queryWrapper.eq(ResourceRequest.FieldNames.createDate.getTableFieldName(),req.getCreateDate());
         ResourceRequest request = new ResourceRequest();
         request.setStatusCd(req.getStatusCd());
         return resourceRequestMapper.update(request,queryWrapper);

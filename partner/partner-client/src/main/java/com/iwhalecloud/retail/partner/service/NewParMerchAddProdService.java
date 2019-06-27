5a2af20abf71c408ab9fb65ec233009c45b6cca7
zhong.wenlong <zhong.wenlong30@iwhalecloud.com>
2019-06-27 17:53:16 +0800

    编辑商家类型为零售商时 设置商家默认权限：a、所有机型经营权限； b、所有地市和所有机型的调拨权限

--- partner/partner-client/src/main/java/com/iwhalecloud/retail/partner/service/NewParMerchAddProdService.java
+++ partner/partner-client/src/main/java/com/iwhalecloud/retail/partner/service/NewParMerchAddProdService.java
@@ -7,7 +7,17 @@ import java.util.List;
 
 public interface NewParMerchAddProdService {
 
-	public List<String> selectProductIdList();
-	
-	public ResultVO<Integer> addProd(LSSAddControlReq req);
+	/**
+	 * 编辑选择零售商时设置默认赋权：a、所有机型经营权限； b、所有地市和所有机型的调拨权限
+	 * @param req
+	 * @return
+	 */
+	ResultVO<Integer> addProd(LSSAddControlReq req);
+
+	/**
+	 * 零售商 设置默认赋权：a、所有机型经营权限； b、所有地市和所有机型的调拨权限
+	 * @param merchantId
+	 * @return
+	 */
+	ResultVO<Integer> addRetailerDefaultRule(String merchantId);
 }

d56d04d7df421c0766afb181f522d7314be58558
liweisong <6027023377@iwhalecloud.com>
2019-07-02 20:56:48 +0800

    地包进销存明细报表

--- report/report-service/src/main/java/com/iwhalecloud/retail/report/service/impl/ReportServiceImpl.java
+++ report/report-service/src/main/java/com/iwhalecloud/retail/report/service/impl/ReportServiceImpl.java
@@ -22,57 +22,13 @@ public class ReportServiceImpl implements ReportService {
 	@Override
 	public ResultVO<Page<ReportDeSaleDaoResq>> getReportDeSaleList(ReportDeSaleDaoReq req) {
 		Page<ReportDeSaleDaoResq> list = (Page<ReportDeSaleDaoResq>) reportManager.ListReportDeSale(req);
-	    
-        return ResultVO.success(list);	
-	}
-	
-	@Override
-    public ResultVO<List<ProductListAllResp>> listProductAll(String brandId){
-	List<ProductListAllResp> list = (List<ProductListAllResp>) reportManager.listProductAll(brandId);
-	    
         return ResultVO.success(list);	
 	}
 
 	@Override
 	public ResultVO<List<ReportDeSaleDaoResq>> reportDeSaleExport(ReportDeSaleDaoReq req) {
 		List<ReportDeSaleDaoResq> list =reportManager.reportDeSaleExport(req);
-		List<ReportDeSaleDaoResq> list2=new ArrayList<ReportDeSaleDaoResq>();	
-
-		for (ReportDeSaleDaoResq rr : list) {
-			String priceLevel = rr.getPriceLevel();
-			String redStatus = rr.getRedStatus();
-			if (priceLevel != null) {
-				if ("1".equals(priceLevel)) {
-					rr.setPriceLevel("400以下");
-				} else if ("2".equals(priceLevel)) {
-					rr.setPriceLevel("400-700");
-				} else if ("3".equals(priceLevel)) {
-					rr.setPriceLevel("700-1000");
-				} else if ("4".equals(priceLevel)) {
-					rr.setPriceLevel("1000-2000");
-				} else if ("5".equals(priceLevel)) {
-					rr.setPriceLevel("2000-3000");
-				} else if (priceLevel.equals("6")) {
-					rr.setPriceLevel("3000以上");
-				}
-			}
-			if (redStatus != null) {
-				if ("1".equals(redStatus)) {
-					rr.setRedStatus("充裕");
-				} else if ("2".equals(redStatus)) {
-					rr.setRedStatus("缺货");
-				} else if ("3".equals(redStatus)) {
-					rr.setRedStatus("严重缺货");
-				}
-			}
-			String typeId = rr.getTypeId();
-			if("201903142030001".equals(typeId)){
-				rr.setTypeId("手机");
-			}
-			list2.add(rr);
-		}
-		// TODO Auto-generated method stub
-		return ResultVO.success(list2);
+		return ResultVO.success(list);
 	}
 
 }

e2e2502295d488d342bb2d82a7c9686cbf46d6b2
liweisong <6027023377@iwhalecloud.com>
2019-07-02 10:49:17 +0800

    地包商定时任务

--- elastic-job-master/src/main/java/cn/buildworld/elasticjob/job/report/RptSupplierOperatingDayJob.java
+++ elastic-job-master/src/main/java/cn/buildworld/elasticjob/job/report/RptSupplierOperatingDayJob.java
@@ -1,34 +1,36 @@
 package cn.buildworld.elasticjob.job.report;
 
+import org.springframework.stereotype.Component;
+
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.dangdang.ddframe.job.api.ShardingContext;
 import com.dangdang.ddframe.job.api.simple.SimpleJob;
 import com.dangdang.elasticjob.lite.annotation.ElasticSimpleJob;
-import com.iwhalecloud.retail.report.service.DataForSuDay;
+import com.iwhalecloud.retail.report.service.RptSupplierOperatingDayService;
+
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
+
 //每天凌晨两点执行定时任务0 0 2 * * ?	每天上午的10点15分执行  0 15 10 ? * *
 @ElasticSimpleJob(cron = "0 0 1 * * ?",
-        jobName = "firstJob",
-        shardingTotalCount = 1,
-        jobParameter = "测试参数",
-        shardingItemParameters = "0=A,1=B",
-        dataSource = "datasource")
+      jobName = "firstJob",
+      shardingTotalCount = 1,
+      jobParameter = "测试参数",
+      shardingItemParameters = "0=A,1=B",
+      dataSource = "datasource")
 @Component
 @Slf4j
-public class ReportDataForSuDayJob implements SimpleJob {
-
+public class RptSupplierOperatingDayJob implements SimpleJob  {
+	
 	@Reference
-    DataForSuDay myJob;
-
-
-    @Override
+    RptSupplierOperatingDayService reportJob;
+	
+	@Override
     public void execute(ShardingContext shardingContext) {
-    	if(myJob == null){
+    	if(reportJob == null){
     		return ;
     	}
 		try {
-			myJob.hqDataForRptSupplierOperatingDay();
+			reportJob.hqRptSupplierOperatingDayData();
 		} catch (Exception e) {
 			log.error("ReportDataForSuDayJob exceute exception", e);
 		}
--- report/report-client/src/main/java/com/iwhalecloud/retail/report/dto/response/MktResInstResq.java
+++ report/report-client/src/main/java/com/iwhalecloud/retail/report/dto/response/MktResInstResq.java
@@ -8,12 +8,15 @@ public class MktResInstResq extends PageVO {/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private String productId;
-	private String productBaseName;
-	private String productName;
-	private String brandId;
-	private String brandName;
-	private String priceLevel;
-	private String productBaseId;
-	private String typeId;
+	private String stockNum;//库存量
+	private String stockAmount;//库存金额
+	private String productId;//产品ID
+	private String typeId;//产品类型
+	private String productBaseName;//机型名称
+	private String productName;//产品名称
+	private String brandId;//品牌ID
+	private String priceLevel;//机型档位
+	private String brandName;//品牌名称
+	private String productBaseId;//机型ID
+	private String createDate;//创建时间
 }

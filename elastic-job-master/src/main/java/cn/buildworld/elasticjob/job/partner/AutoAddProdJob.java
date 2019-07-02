ef74819c9f42ecd26c1c7e66429500d71c9c9e79
yao.yucheng <yao.yucheng@iwhalecloud.com>
2019-07-02 15:00:05 +0800

    修改添加商家权限后台任务执行时间

--- elastic-job-master/src/main/java/cn/buildworld/elasticjob/job/partner/AutoAddProdJob.java
+++ elastic-job-master/src/main/java/cn/buildworld/elasticjob/job/partner/AutoAddProdJob.java
@@ -28,7 +28,7 @@ import java.util.List;
 /**
  * Created by Administrator on 2019/6/18.
  */
-@ElasticSimpleJob(cron = "0 0 20 * * ?",
+@ElasticSimpleJob(cron = "0 30 15 * * ?",
         jobName = "AutoAddProdJob",
         shardingTotalCount = 1,
         jobParameter = "测试参数",

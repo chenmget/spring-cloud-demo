d7f5b27e7ac3df5b96831ac78e79a68eeb928dcf
he.shaowu <he.shaowu01@iwhalecloud.com>
2019-07-03 21:47:44 +0800

    fix 厂商串码入库重复校验bug

--- warehouse/warehouse-service/src/main/java/com/iwhalecloud/retail/warehouse/service/impl/AdminResourceInstServiceImpl.java
+++ warehouse/warehouse-service/src/main/java/com/iwhalecloud/retail/warehouse/service/impl/AdminResourceInstServiceImpl.java
@@ -47,7 +47,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -761,10 +760,12 @@ public class AdminResourceInstServiceImpl implements AdminResourceInstService {
         if (CollectionUtils.isEmpty(instListResps)) {
             return ResultVO.error(constant.getNoResInst());
         }
+        List<String> mktResInstNbrList = new ArrayList<>(instListResps.size());
         for (ResourceInstDTO resp : instListResps) {
             if (StringUtils.isNotEmpty(resp.getOrderId())) {
                 return ResultVO.error(resp.getMktResInstNbr()+constant.getTradeNbrCanNotReset());
             }
+            mktResInstNbrList.add(resp.getMktResInstNbr());
         }
 
         String mktResStoreId = resouceInstTrackDetailManager.getMerchantStoreId(instListResps.get(0).getMktResInstNbr());
@@ -787,17 +788,21 @@ public class AdminResourceInstServiceImpl implements AdminResourceInstService {
         if (!resultVO.isSuccess()) {
             return resultVO;
         }
+        if (CollectionUtils.isEmpty(mktResInstNbrList)) {
+            return resultVO;
+        }
         // 更新厂家对应的串码
-        AdminResourceInstDelReq delReq = new AdminResourceInstDelReq();
-        delReq.setUpdateStaff(req.getUpdateStaff());
-        delReq.setMktResInstIdList(req.getMktResInstIdList());
-        req.setStatusCd(ResourceConst.STATUSCD.AVAILABLE.getCode());
-        delReq.setEventType(ResourceConst.EVENTTYPE.NO_RECORD.getCode());
-        delReq.setMktResStoreId(req.getDestStoreId());
-        delReq.setDestStoreId(mktResStoreId);
+        ResourceInstUpdateReq updateReq = new ResourceInstUpdateReq();
+        updateReq.setUpdateStaff(req.getUpdateStaff());
+        updateReq.setMktResInstNbrs(mktResInstNbrList);
+        updateReq.setStatusCd(ResourceConst.STATUSCD.AVAILABLE.getCode());
+        updateReq.setMktResStoreId(req.getMktResStoreId());
+        updateReq.setDestStoreId(mktResStoreId);
         List<String> checkStatusCd = Lists.newArrayList(ResourceConst.STATUSCD.DELETED.getCode());
-        delReq.setCheckStatusCd(checkStatusCd);
+        updateReq.setCheckStatusCd(checkStatusCd);
 
-        return resourceInstService.updateResourceInstByIds(delReq);
+        Integer sucessNum = resourceInstManager.batchUpdateInstState(updateReq);
+        log.info("AdminResourceInstServiceImpl.resetResourceInst resourceInstManager.batchUpdateInstState req={}, resp={}", JSON.toJSONString(updateReq), sucessNum);
+        return resultVO;
     }
 }

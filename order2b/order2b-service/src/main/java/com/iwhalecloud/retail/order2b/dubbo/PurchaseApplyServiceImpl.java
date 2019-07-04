0e0786df9eed6cce27c838d988cc4486d73d38db
chenbin748 <chen.bin748@iwhalecloud.com>
2019-07-04 14:37:37 +0800

    service注解

--- order2b/order2b-service/src/main/java/com/iwhalecloud/retail/order2b/dubbo/PurchaseApplyServiceImpl.java
+++ order2b/order2b-service/src/main/java/com/iwhalecloud/retail/order2b/dubbo/PurchaseApplyServiceImpl.java
@@ -3,6 +3,7 @@ package com.iwhalecloud.retail.order2b.dubbo;
 import com.alibaba.dubbo.config.annotation.Reference;
 
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.google.common.collect.Lists;
@@ -32,7 +33,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.stereotype.Service;
+
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
